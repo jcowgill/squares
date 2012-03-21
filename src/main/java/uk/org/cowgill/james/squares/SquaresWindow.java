2db6ecb908daa2eafc2c5f5e73d9172940b6c587
James Cowgill <james410@cowgill.org.uk>
2012-03-21 23:42:37 +0000

    Added SquaresWindow class

---
+++ src/main/java/uk/org/cowgill/james/squares/SquaresWindow.java
@@ -0,0 +1,19 @@
+package uk.org.cowgill.james.squares;
+
+import javax.swing.JFrame;
+
+/**
+ * The top level window for the squares program
+ * 
+ * @author james
+ */
+public class SquaresWindow extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
