4fbe2ab2c357f2fb05b22e16d546ced74aef480f
James Cowgill <james410@cowgill.org.uk>
2012-03-29 22:34:42 +0100

    Implements the game painter in PanelGame.GameCanvas

--- src/main/java/uk/org/cowgill/james/squares/PanelGame.java
+++ src/main/java/uk/org/cowgill/james/squares/PanelGame.java
@@ -1,10 +1,14 @@
 package uk.org.cowgill.james.squares;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,6 +19,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.event.MouseInputAdapter;
 
 /**
  * The JPanel which displays the visual elements of the game
@@ -25,6 +30,9 @@ public class PanelGame extends JPanel
 {
 	private static final long serialVersionUID = 1L;
 
+	//Controller
+	private final GameController ctrl;
+	
 	//Form controls
 	private final JLabel lblPlayer[] = new JLabel[2];
 	private final JLabel lblScore[] = new JLabel[2];
@@ -38,7 +46,7 @@ public class PanelGame extends JPanel
 	 * 
 	 * @param rootPane root pane to set default button of (or null)
 	 */
-	public PanelGame(JRootPane rootPane)
+	public PanelGame(JRootPane rootPane, GameController ctrl)
 	{
 		setLayout(new BorderLayout(0, 0));
 		
@@ -94,6 +102,9 @@ public class PanelGame extends JPanel
 		{
 			rootPane.setDefaultButton(sendButton);
 		}
+		
+		//Controller
+		this.ctrl = ctrl;
 	}
 
 	/**
@@ -122,18 +133,258 @@ public class PanelGame extends JPanel
 	 * 
 	 * @author james
 	 */
-	private static class GameCanvas extends JPanel
+	private class GameCanvas extends JPanel
 	{
 		private static final long serialVersionUID = 1L;
 		
-		//State of game
+		//Position and Size Constants
+		private static final int PADDING = 16;
+		private static final int DOT_SIZE = 10;
+		private static final int SQUARE_SIZE = 48;
+		private static final int SQUARES = 8;
 
+		//Game area size without padding
+		private static final int CONTENT_SIZE = SQUARE_SIZE * SQUARES + DOT_SIZE;
+		
+		//Stroke used to draw lines with
+		private final Stroke lineStroke = new BasicStroke(4);
+		
+		//Square colours
+		private final Color[] squareColour = new Color[]{
+				Color.LIGHT_GRAY, Color.BLUE, Color.RED
+		};
+		
+		//Game State
+		private GameState state = new GameState(8, true);
+		
+		//Coordinates for line to draw mouse hovering on
+		private int lastMouseX;
+		private int lastMouseY;
+		private boolean lastMouseIsLeft;
+		private boolean lastMouseEnabled = false;
+		
+		/**
+		 * Creates a new game canvas
+		 */
 		public GameCanvas()
 		{
 			super(true);
-			setPreferredSize(new Dimension(300, 300));
+			
+			//Setup panel
+			setPreferredSize(new Dimension(PADDING * 2 + CONTENT_SIZE, PADDING * 2 + CONTENT_SIZE));
 			setBorder(new LineBorder(Color.BLACK));
-			setBackground(Color.BLUE);
+			setBackground(squareColour[0]);
+			
+			//Add line hovering handler
+			MouseInputAdapter mouseListener = new MouseInputAdapter()
+			{
+				@Override
+				public void mouseClicked(MouseEvent e)
+				{
+					//Ensure hover is in correct position
+					mouseMoved(e);
+					
+					//TODO make move
+					repaint();
+				}
+				
+				@Override
+				public void mouseExited(MouseEvent e)
+				{
+					//Disable hover
+					GameCanvas.this.lastMouseEnabled = false;
+					repaint();
+				}
+				
+				@Override
+				public void mouseMoved(MouseEvent e)
+				{
+					//Calulate the square and where inside the square we are
+					int x 		= (e.getX() - PADDING - DOT_SIZE / 2) / SQUARE_SIZE;
+					int xInside	= (e.getX() - PADDING - DOT_SIZE / 2) % SQUARE_SIZE;
+					int y 		= (e.getY() - PADDING - DOT_SIZE / 2) / SQUARE_SIZE;
+					int yInside	= (e.getY() - PADDING - DOT_SIZE / 2) % SQUARE_SIZE;
+					boolean isLeft;
+					
+					//Check for padding positions
+					if(x == 8)
+					{
+						//Bottom right corner?
+						if(y == 8)
+						{
+							//Which way to move up?
+							if(xInside < yInside)
+							{
+								x--;
+								isLeft = false;
+							}
+							else
+							{
+								y--;
+								isLeft = true;
+							}
+						}
+						else
+						{
+							//Force left
+							isLeft = true;
+						}
+					}
+					else
+					{
+						//Bottom side
+						if(y == 8)
+						{
+							isLeft = false; 
+						}
+						else
+						{
+							//MAIN TESTER
+							//Which of the 4 surrounding lines is the nearest?
+							if(SQUARE_SIZE - xInside < yInside)
+							{
+								//Bottom right
+								if(xInside < yInside)
+								{
+									//Bottom
+									y++;
+									isLeft = false;
+								}
+								else
+								{
+									//Right
+									x++;
+									isLeft = true;
+								}
+							}
+							else
+							{
+								//On current square - to the left?
+								isLeft = xInside < yInside;
+							}							
+						}
+					}
+					
+					//Has it changed?
+					if(!GameCanvas.this.lastMouseEnabled ||
+							x != GameCanvas.this.lastMouseX ||
+							y != GameCanvas.this.lastMouseY ||
+							isLeft != GameCanvas.this.lastMouseIsLeft)
+					{
+						//Store last mouse location
+						GameCanvas.this.lastMouseX = x;
+						GameCanvas.this.lastMouseY = y;
+						GameCanvas.this.lastMouseIsLeft = isLeft;
+						GameCanvas.this.lastMouseEnabled = true;
+						
+						//Repaint
+						repaint();
+					}
+				}
+			};
+			
+			addMouseListener(mouseListener);
+			addMouseMotionListener(mouseListener);
+		}
+		
+		/**
+		 * Sets the game state of this canvas
+		 * 
+		 * @param state the new state object or null to display nothing
+		 */
+		public void setGameState(GameState state)
+		{
+			this.state = state;
+			repaint();
+		}
+		
+		@Override
+		protected void paintComponent(Graphics gOld)
+		{
+			//Paint base of object
+			super.paintComponent(gOld);
+			
+			//Repaint game window
+			if(state != null)
+			{
+				Graphics2D g = (Graphics2D) gOld.create(PADDING, PADDING, CONTENT_SIZE, CONTENT_SIZE);
+
+				//Fill squares
+				for(int x = 0; x < 8; x++)
+				{
+					for(int y = 0; y < 8; y++)
+					{
+						g.setColor(squareColour[state.getSquareColour(x, y)]);
+						g.fillRect(x * SQUARE_SIZE + DOT_SIZE / 2,
+									y * SQUARE_SIZE + DOT_SIZE / 2,
+									SQUARE_SIZE, SQUARE_SIZE);
+					}
+				}
+				
+				//Paint game lines
+				g.setStroke(lineStroke);
+				g.setColor(Color.DARK_GRAY);
+				
+				// Hover lines
+				if(lastMouseEnabled)
+				{
+					int baseX = lastMouseX * SQUARE_SIZE + DOT_SIZE / 2;
+					int baseY = lastMouseY * SQUARE_SIZE + DOT_SIZE / 2;
+					
+					if(lastMouseIsLeft)
+					{
+						//Draw left line
+						g.drawLine(baseX, baseY, baseX, baseY + SQUARE_SIZE);
+					}
+					else
+					{
+						//Draw top line
+						g.drawLine(baseX, baseY, baseX + SQUARE_SIZE, baseY);
+					}
+				}
+				
+				// Top lines
+				g.setColor(Color.BLACK);
+				for(int x = 0; x < 8; x++)
+				{
+					for(int y = 0; y <= 8; y++)
+					{
+						if(state.getTopLine(x, y))
+						{
+							g.drawLine(x * SQUARE_SIZE + DOT_SIZE / 2,
+										y * SQUARE_SIZE + DOT_SIZE / 2,
+										(x + 1) * SQUARE_SIZE + DOT_SIZE / 2,
+										y * SQUARE_SIZE + DOT_SIZE / 2);
+						}
+					}
+				}
+				
+				// Left lines
+				for(int x = 0; x <= 8; x++)
+				{
+					for(int y = 0; y < 8; y++)
+					{
+						if(state.getLeftLine(x, y))
+						{
+							g.drawLine(x * SQUARE_SIZE + DOT_SIZE / 2,
+										y * SQUARE_SIZE + DOT_SIZE / 2,
+										x * SQUARE_SIZE + DOT_SIZE / 2,
+										(y + 1) * SQUARE_SIZE + DOT_SIZE / 2);
+						}
+					}
+				}
+				
+				//Paint dots
+				for(int x = 0; x <= 8; x++)
+				{
+					for(int y = 0; y <= 8; y++)
+					{
+						g.fillOval(SQUARE_SIZE * x,
+									SQUARE_SIZE * y,
+									DOT_SIZE, DOT_SIZE);
+					}
+				}
+			}
 		}
 	}
 }
